58e48bf98428240d5890328075464d44068a533b
varuneti <132238912+varuneti@users.noreply.github.com>
2023-12-21 22:18:51 -0800

    Add files via upload

---
+++ WishBeltExam/wishApp/main/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+import re
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+class UserManager(models.Manager):
+    def registration_validator(self, postData):
+        errors = {}
+        if len(postData['first_name']) < 1:
+            errors['first_name'] = "Name must be at least 1 character"
+        if len(postData['last_name']) < 1:
+            errors['last)name'] = "Alias must be at least 1 character"
+        if not EMAIL_REGEX.match(postData['email']):   
+            errors['email'] = "Invalid email address!"
+        users_with_email = User.objects.filter(email = postData['email'])
+        if len(users_with_email) >= 1:
+            errors['duplicate'] = "Email already exists."
+        if len(postData['password']) < 4:
+            errors['password'] = "Password must be at least 4 characters"
+        if postData['password'] != postData['confirm_password']:
+            errors['pw_match'] = "Password must match!"
+        return errors
+
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    objects = UserManager()
+
+
+
+# Create your models here.
---
+++ WishBeltExam/wishApp/main/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, HttpResponse
+
+def index(request):
+    return HttpResponse("Hello")
+
+
+
+# Create your views here.
